c797b2ab7f780f3bc658a3a4eb658bba891b8af5
Rafal Kociszewski <rafal.kociszewski@gmail.com>
2019-02-14 16:20:34 +0100

    add Atom uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+rm -rf ~/.atom
+rm /usr/local/bin/atom
+rm /usr/local/bin/apm
+rm ~/Library/Preferences/com.github.atom.plist
+rm ~/Library/Application Support/com.github.atom.ShipIt
+rm -rf ~/Library/Application Support/Atom/
